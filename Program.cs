a8bc5eceaa0ac0351bd47df870ff12a3fa91144d
Andrew <Andy>
2023-08-04 21:40:55 +0300

    DZ1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+if (firstnumber > secondnumber)
+{
+Console.Write("max = " + firstnumber + ", min = " + secondnumber);
+}
+else if (firstnumber < secondnumber)
+{Console.Write("max = "+ secondnumber + ", min = " + firstnumber);}
+else 
+{
+Console.Write("max = min = " + firstnumber);
+}
